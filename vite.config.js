4e903035eaeead2c74d7c2ed2f2ddf921e0b6a71
safeer1748 <safeerkhan1748@gmail.com>
2024-08-03 15:15:16 +0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/My_Portfolio/",
+  base: "/my_portfolio/",
   plugins: [react()],
 })
